58aa99c87a19e5891780d1dcbdde251800e75d42
WooLeeHappy <118141462+WooLeeHappy@users.noreply.github.com>
2022-12-02 20:46:46 +0900

    수정했대 진짜로야

---
+++ src/main/java/com/sparta/myselectshopbeta/dto/ProductMypriceRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshopbeta.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
